bc1eb339716b3fa51fe7f86fac7a9f1be38e3e23
Marcos QP Dell Personal <marcos.quispe.sb@outlook.com>
2024-03-25 23:27:02 -0400

    commit init

---
+++ src/main/java/org/example/algoritmica/Main.java
@@ -0,0 +1,14 @@
+package org.example.algoritmica;
+
+/**
+ * Main
+ *
+ * @author Marcos Quispe
+ * @since 1.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/org/example/algoritmica/others/Clase0.java
@@ -0,0 +1,32 @@
+// Nota: al copiar la clase a su proyecto nuevo, actualice la sig linea para que tenga el package correcto
+package org.example.algoritmica.others;
+
+/**
+ * Clase0
+ *
+ * @author Marcos Quispe
+ * @since 1.0
+ */
+public class Clase0 {
+
+    public int frequency(int[] values, int valueToSearch) {
+        return 0;
+    }
+
+    public void insertionSort(int[] values) {
+        //implement
+    }
+
+    public void selectionSort(int[] values) {
+        // implement
+    }
+
+    public boolean brackets(String line) {
+        // implement
+        return false;
+    }
+
+    public static void main(String[] args) {
+        // en la revision se pasara todo el contenido main para poner a prueba sus algoritmos
+    }
+}
