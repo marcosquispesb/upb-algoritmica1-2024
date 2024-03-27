d30ebfbb95bc3e7155dbdfccabe1214e2e12e4db
Marcos QP Dell Personal <marcos.quispe.sb@outlook.com>
2024-03-27 02:49:00 -0400

    metodos bases preparados para bubble sort mejorado y radix sort de numeros

--- src/main/java/org/example/algoritmica/ordering/Ordering.java
+++ src/main/java/org/example/algoritmica/ordering/Ordering.java
@@ -1,6 +1,7 @@
 package org.example.algoritmica.ordering;
 
 import java.util.Arrays;
+import java.util.NoSuchElementException;
 
 /**
  * Ordering
@@ -10,6 +11,7 @@ import java.util.Arrays;
  */
 public class Ordering {
 
+    //region [Bubble Sort] --------------------------------------------------------------------------
     public void bubbleSort(int[] values) {
         for (int limit = values.length - 2; limit > 0 ; limit--) {
             for (int i = 0; i <= limit; i++) {
@@ -19,19 +21,55 @@ public class Ordering {
         }
     }
 
+    public void bubbleSortImproved(int[] values) {
+
+    }
+
     // funcion que permite intercambiar elementos de un array
     private void swap(int[] values, int i, int j) {
         int aux = values[i];
         values[i] = values[j];
         values[j] = aux;
     }
+    //endregion
+
+    //region [Radix Sort] --------------------------------------------------------------------------
+    public static int max(int[] values) {
+        int max = Integer.MIN_VALUE;
+        for (int value : values) {
+            if (value > max)
+                max = value;
+        }
+        return max;
+    }
+
+    public static int countDigits(int nro) {
+        int length = 0;
+        while (nro > 0) {
+            nro = nro / 10;
+            length++;
+        }
+        return length;
+    }
+
+    public static void radixSort(int[] values) {
+
+    }
+    //endregion
 
     public static void main(String[] args) {
         Ordering ordering = new Ordering();
         int[] values;
 
         values = new int[] {3, 0, 1, 4, 2, 5, 1};
+        //values = new int[] {0, 1, 1, 2, 3, 4, 5};
         ordering.bubbleSort(values);
+        System.out.println(Arrays.toString(values));
+
+        // -----------------------------------------------
+
+        values = new int[] {352, 62, 56, 3, 16, 353};
+        radixSort(values);
         System.out.println(Arrays.toString(values));
     }
 }
